71277d2d417bee79e2909734ff0308976bb6b1ca
titancheg <87692055+titancheg@users.noreply.github.com>
2021-07-20 10:50:59 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"bioinvn@ya.ru\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "
-[server]\n
-headless = true\n
-port = $PORT\n
-enableCORS = false\n
-\n
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
